0132ecc2513faf2495b20bff3d53d946fe5a0cc3
holasoymas <71905956331mahesh@gmail.com>
2025-07-02 15:43:56 +0545

    login route added

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class LoginController extends Controller
+{
+    public function create()
+    {
+        return view("auth.login");
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // regenerate the session id
+            $request->session()->regenerate();
+
+            return to_route("user.show", ["user" => Auth::id()]);
+        }
+
+        return back()->withErrors([
+            'error' => 'Invalid credentials',
+        ])->onlyInput('email'); // prepolute email field
+    }
+}
