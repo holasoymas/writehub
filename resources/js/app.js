bbbae93a8d54794e83f928647c4eed32ea03f991
holasoymas <71905956331mahesh@gmail.com>
2025-07-24 15:12:13 +0545

    add : automitically include x-csrf token

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import axios from 'axios';
+
+// Get the token from meta tag
+const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+// Set it in Axios headers
+if (token) {
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
+} else {
+    console.error('CSRF token not found in meta tag!');
+}
