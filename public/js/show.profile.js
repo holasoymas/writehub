efe3db6873431954078439b46f1b0337c8b4c8d2
holasoymas <71905956331mahesh@gmail.com>
2025-07-24 15:11:04 +0545

    refactoring of code

--- public/js/show.profile.js
+++ public/js/show.profile.js
@@ -1,19 +1,6 @@
 
 // Mobile menu toggle
 document.addEventListener('DOMContentLoaded', () => {
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    if ($navbarBurgers.length > 0) {
-        $navbarBurgers.forEach(el => {
-            el.addEventListener('click', () => {
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
-
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-            });
-        });
-    }
 
     document.getElementById('show-followers').addEventListener('click', showFollowersModal);
     document.getElementById('show-followings').addEventListener('click', showFollowingModal);
