20d73fd70908f8a81515eaf5ed0377c9dfc28b21
holasoymas <71905956331mahesh@gmail.com>
2025-10-21 22:50:04 +0545

    added : comment factory

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'body' => $this->faker->paragraph,
+            'user_id' => User::inRandomOrder()->first()->id,
+            'post_id' => Post::inRandomOrder()->first()->id,
+            'parent_id' => Comment::inRandomOrder()->first()->id,
+        ];
+    }
+}
