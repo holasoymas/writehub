a9408af581b61503a96d72b07ec7fc651e90ef9a
holasoymas <71905956331mahesh@gmail.com>
2025-10-29 17:25:35 +0545

    added : fillable field for report

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Report extends Model
 {
+
+    protected $fillable = ['post_id', 'user_id', 'reason', 'other'];
+
     public function blog(): BelongsTo
     {
         return $this->belongsTo(Post::class);
