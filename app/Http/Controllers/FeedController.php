dd128183a826cb72f5d4c5b4a7ee509ac5876b8d
holasoymas <71905956331mahesh@gmail.com>
2025-11-19 18:52:50 +0545

    added : Feed ranking for feed page

---
+++ app/Http/Controllers/FeedController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Tag;
+use App\Services\FeedRankingService;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class FeedController extends Controller
+{
+    public function show()
+    {
+        $user = Auth::user();
+
+        $trendingBlogs = $this->getTrendingBlogs();
+
+        $posts = Post::withCount(['likes', 'comments'])
+            ->with('user', 'tags')->get();
+        // Log::info(json_encode($posts, JSON_PRETTY_PRINT));
+
+        $rankingPosts = (new FeedRankingService())->rankPosts($posts, $user);
+        foreach ($rankingPosts as $posts) {
+            Log::info($posts->final_score);
+        }
+
+        $tags = (new Tag())->getRecommendedTags($user, 10);
+
+        return view('welcome', compact("trendingBlogs", "rankingPosts", "tags"));
+    }
+
+    public function getTrendingBlogs()
+    {
+        $oneWeekAgo = Carbon::now()->subDays(7);
+
+        $trendingBlogs = Post::with('user')
+            ->withCount([
+                'likes as recent_likes' => function ($query) use ($oneWeekAgo) {
+                    $query->where('created_at', '>=', $oneWeekAgo);
+                },
+                'comments as recent_comments' => function ($query) use ($oneWeekAgo) {
+                    $query->where('created_at', '>=', $oneWeekAgo);
+                }
+            ])
+            ->get()
+            ->map(function ($blog) {
+                // calculate score
+                $blog->trending_score = $blog->recent_likes + ($blog->recent_comments * 2);
+
+                return $blog;
+            })
+            ->sortByDesc('trending_score')
+            ->take(6);
+
+        return $trendingBlogs;
+    }
+}
