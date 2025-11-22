c666d4d4e9b7c87983eda44ca6c1317eff4ffd97
holasoymas <71905956331mahesh@gmail.com>
2025-11-22 16:03:27 +0545

    added : migration for broadcast announcements

---
+++ database/migrations/2025_11_22_080239_create_broadcasts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('broadcasts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('message');
+            $table->integer('total_send');
+            $table->timestamp('send_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('broadcasts');
+    }
+};
